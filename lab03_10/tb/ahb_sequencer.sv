f770e467d4e35d29f372e3ccfd368e48dfcb276c
khizramuzaffar12 <khizra.muzaffar@impare.cloud>
2026-01-29 11:19:11 +0000

    axi4_lite_master

--- lab03_10/tb/ahb_sequencer.sv
+++
@@ -1,7 +0,0 @@
-class ahb_sequencer extends uvm_sequencer #(ahb_txn);
-  `uvm_component_utils(ahb_sequencer)
-  function new(string name, uvm_component parent);
-    super.new(name,parent);
-  endfunction
-endclass
-
