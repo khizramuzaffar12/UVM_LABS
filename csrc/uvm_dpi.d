38407cd25b1d839e01c332b5711d6cdacd798a30
khizramuzaffar12 <khizra.muzaffar@impare.cloud>
2026-01-29 07:21:42 +0000

    Add AXII_Master files

--- csrc/uvm_dpi.d
+++
@@ -1,9 +0,0 @@
-uvm_dpi.o: /opt/synopsys/vcs/W-2024.09-SP2/etc/uvm/dpi/uvm_dpi.cc \
- /opt/synopsys/vcs/W-2024.09-SP2/etc/uvm/dpi/uvm_regex.cc \
- /opt/synopsys/vcs/W-2024.09-SP2/include/vpi_user.h \
- /opt/synopsys/vcs/W-2024.09-SP2/etc/uvm/dpi/uvm_hdl.c \
- /opt/synopsys/vcs/W-2024.09-SP2/include/sv_vpi_user.h \
- /opt/synopsys/vcs/W-2024.09-SP2/include/vpi_user.h \
- /opt/synopsys/vcs/W-2024.09-SP2/include/veriuser.h \
- /opt/synopsys/vcs/W-2024.09-SP2/include/svdpi.h \
- /opt/synopsys/vcs/W-2024.09-SP2/etc/uvm/dpi/uvm_svcmd_dpi.c
